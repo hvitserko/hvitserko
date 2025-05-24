509e29c455cd4b4e4306cd0aae1c333501b743b0
giovani <giovanipereira143@gmail.com>
2025-05-24 11:21:06 -0300

    teste

---
+++ guia/programacao.py
@@ -0,0 +1,77 @@
+import os
+import gzip
+import xml.etree.ElementTree as ET
+import requests
+
+save_as_gz = False  # Set to True to save an additional .gz version
+
+tvg_ids_file = os.path.join(os.path.dirname(__file__), 'id.txt')
+output_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'mylist.xml')
+output_file_gz = output_file + '.gz'
+
+def fetch_and_extract_xml(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+        print(f"Failed to fetch {url}")
+        return None
+
+    if url.endswith('.gz'):
+        try:
+            decompressed_data = gzip.decompress(response.content)
+            return ET.fromstring(decompressed_data)
+        except Exception as e:
+            print(f"Failed to decompress and parse XML from {url}: {e}")
+            return None
+    else:
+        try:
+            return ET.fromstring(response.content)
+        except Exception as e:
+            print(f"Failed to parse XML from {url}: {e}")
+            return None
+
+def filter_and_build_epg(urls):
+    with open(tvg_ids_file, 'r') as file:
+        valid_tvg_ids = set(line.strip() for line in file)
+
+    root = ET.Element('tv')
+
+    for url in urls:
+        epg_data = fetch_and_extract_xml(url)
+        if epg_data is None:
+            continue
+
+        for channel in epg_data.findall('channel'):
+            tvg_id = channel.get('id')
+            if tvg_id in valid_tvg_ids:
+                root.append(channel)
+
+        for programme in epg_data.findall('programme'):
+            tvg_id = programme.get('channel')
+            if tvg_id in valid_tvg_ids:
+                root.append(programme)
+
+    tree = ET.ElementTree(root)
+    tree.write(output_file, encoding='utf-8', xml_declaration=True)
+    print(f"Salvo em {output_file}")
+
+    if save_as_gz:
+        with gzip.open(output_file_gz, 'wb') as f:
+            tree.write(f, encoding='utf-8', xml_declaration=True)
+        print(f"Salvo em {output_file_gz}")
+	    
+fontes = os.getenv("FONTES")
+urls = [
+        'https://epgshare01.online/epgshare01/epg_ripper_US1.xml.gz',
+        'https://raw.githubusercontent.com/matthuisman/i.mjh.nz/refs/heads/master/Plex/us.xml',
+        'https://raw.githubusercontent.com/matthuisman/i.mjh.nz/refs/heads/master/PlutoTV/br.xml',
+        'https://raw.githubusercontent.com/matthuisman/i.mjh.nz/refs/heads/master/PlutoTV/us.xml',
+        'https://raw.githubusercontent.com/matthuisman/i.mjh.nz/refs/heads/master/SamsungTVPlus/us.xml',
+        'https://raw.githubusercontent.com/matthuisman/i.mjh.nz/refs/heads/master/SamsungTVPlus/gb.xml',
+        'https://raw.githubusercontent.com/matthuisman/i.mjh.nz/refs/heads/master/SamsungTVPlus/ca.xml',
+        'https://raw.githubusercontent.com/matthuisman/i.mjh.nz/refs/heads/master/PlutoTV/ca.xml',
+        'https://raw.githubusercontent.com/matthuisman/i.mjh.nz/refs/heads/master/PlutoTV/gb.xml',
+        'https://raw.githubusercontent.com/rootcoder/epgtv/main/guide.xml.gz',
+]
+
+if __name__ == "__main__":
+    filter_and_build_epg(urls)
